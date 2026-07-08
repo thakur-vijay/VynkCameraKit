ec61804e8771c5e102a4c2cb4ce3079f97169466
ThakurVijay210901 <105584645+ThakurVijay2191@users.noreply.github.com>
2026-07-08 23:45:15 +0530

    Initial VynkCameraKit package setup

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.4
+
+import PackageDescription
+
+let package = Package(
+    name: "VynkCameraKit",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "VynkCameraKit",
+            targets: ["VynkCameraKit"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "VynkCameraKit",
+            dependencies: [
+                "CameraEngine"
+            ]
+        ),
+        .binaryTarget(
+            name: "CameraEngine",
+            url: "https://github.com/thakur-vijay/CameraEngine/releases/download/1.0.0/CameraEngine.xcframework.zip",
+            checksum: "bab8c9794e0081933a73ec3f1ba24506c6ddcadc3717b730ffe222eaa933dcf3"
+        )
+    ]
+)
